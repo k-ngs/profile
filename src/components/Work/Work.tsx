449550d933bb37388f35720b98570333b2c085b0
Kohei Nagase <k.nagase.biz@gmail.com>
2020-10-02 23:24:07 +0900

    Add work contents

--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import "./Work.css";
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
 
 const Work = () => {
     return (
-        <div>
-            <p>Work page</p>
+        <div className="work-contents">
+            <div className="resume">
+                <h2>職務経歴</h2>
+                <p>
+                    大学でコンピュータ・サイエンスやソフトウェアエンジニアリングなどについて学ぶ。<br/>
+                    卒業後はISPにて、自社のクラウドストレージサービスの開発・運用に従事。サーバサイドアプリケーションの開発や障害対応、サーバリプレースなどの運用作業など担当。
+                </p>
+            </div>
+            <div>
+                <p>ソースコードなどの成果物はgithubをご覧ください。</p>
+                <a href="https://github.com/k-ngs">
+                    <FontAwesomeIcon icon={faGithub} size={"4x"} color="black" />
+                </a>
+            </div>
         </div>
     );
 };
